c4a5e96a142d80392561529359f46b110514d7cc
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-08-19 15:25:08 -0600

    moved routes to routes/ideaRoutes.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import ideaRouter from './routes/ideaRoutes.js';
 
 dotenv.config();
 
@@ -8,6 +9,15 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+
+// accept raw json
+app.use(express.json());
+
+// accept form data
+app.use(express.urlencoded({ extended: true }));
+
+// Map /api/ideas to ideaRoutes file
+app.use('/api/ideas', ideaRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
