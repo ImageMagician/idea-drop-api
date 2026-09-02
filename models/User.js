6dbd186f338acc24bf8bac886dc2f51a760e576b
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 11:40:04 -0600

    models/User.js: fixed async "next" issue on .pre('save')

--- models/User.js
+++ models/User.js
@@ -29,11 +29,10 @@ const userSchema = new mongoose.Schema(
 
 // Hash passwords before save
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+    if (!this.isModified("password")) return;
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 })
 
 const User = mongoose.model("User", userSchema);
