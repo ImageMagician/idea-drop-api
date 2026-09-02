9f285b851ec10724580771440f55beddf96cabdb
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 11:59:37 -0600

    utils/generateToken.js added accessToken, refreshToken to register

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/User.js';
+import { generateToken } from "../utils/generateToken.js";
+import * as jwt from "node/crypto.d.ts";
 
 const router = express.Router();
 
@@ -28,7 +30,24 @@ router.post('/register', async (req, res, next) => {
             name, email, password
         });
 
+        // Create tokens
+        const payload = {
+            userId: user._id.toString()
+        }
+
+        const accessToken = await generateToken(payload, '1m');
+        const refreshToken = await generateToken(payload, '30d');
+
+        // Set refresh token in HTTP-Only cookie
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none',
+            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+        });
+
         res.status(201).json({
+            accessToken,
             user: {
                 id: user._id,
                 name: user.name,
