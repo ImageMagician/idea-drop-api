68d70a95db7c13fd47b2c27525a3109ad6234b9f
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 11:06:27 -0600

    server.js: added authRouter

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import ideaRouter from './routes/ideaRoutes.js';
+import authRouter from './routes/authRoutes.js';
 import errorHandler from './middleware/errorHandler.js';
 import connectDB from './config/db.js';
 
@@ -23,6 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Map /api/ideas to ideaRoutes file
 app.use('/api/ideas', ideaRouter);
+app.use('/api/auth', authRouter);
 
 // 404 Fallback
 app.use((req, res, next) => {
