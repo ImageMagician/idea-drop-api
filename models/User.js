5e2ad6c53585a887b3d64d9cc4d20d005d9be07f
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 11:02:06 -0600

    models/User.js: added password hash with bcrypt

--- models/User.js
+++ models/User.js
@@ -27,6 +27,15 @@ const userSchema = new mongoose.Schema(
     }
 );
 
+// Hash passwords before save
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
