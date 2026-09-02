d75fbec957a47f86b3f19386875713abb427f66f
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 13:27:47 -0600

    models/User.js matchPassword added

--- models/User.js
+++ models/User.js
@@ -33,7 +33,12 @@ userSchema.pre("save", async function (next) {
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-})
+});
+
+// Compare entered password with hashed password
+userSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 
